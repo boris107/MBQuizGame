4bc23a550b30d57a359b02bab7f4c1926f6c6b9f
b.klimchuk <borisklimchuk@gmail.com>
2025-01-30 14:04:06 +0300

    Добавить историю ответов
    - Добавлена модель AnswerHistory для истории ответов

---
+++ Data/Models/AnswerHistory.cs
@@ -0,0 +1,12 @@
+﻿namespace Data.Models
+{
+    public class AnswerHistory
+    {        
+        public int AnswerHistoryId { get; set; }
+        public int SessionId { get; set; }
+        public int PlayerId { get; set; }
+        public int QuetionId { get; set; }
+        public int AnswerId { get; set; }
+        public DateTime DateTimeAnswered { get; set; }
+    }
+}
