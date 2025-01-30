d71aa574b973d06ca65bf825c4ebc613f21eb18d
b.klimchuk <borisklimchuk@gmail.com>
2025-01-30 14:07:06 +0300

    Добавить информацию модели Session
    - Добавлена информация времени о создании, старта, окончания игры
    - Добавлена информация о игроке, который создал сессию и выиграл ее

--- Data/Models/Session.cs
+++ Data/Models/Session.cs
@@ -1,10 +1,18 @@
-﻿namespace Data.Models
+﻿using System.Data.Common;
+using System;
+
+namespace Data.Models
 {
     public class Session
     {
         public int SessionId { get; set; }
         public string SessionName { get; set; }
         public string Status { get; set; }
+        public DateTime DateTimeCreated { get; set; }
+        public DateTime DateTimeStarted { get; set; }
+        public DateTime DateTimeEnded { get; set; }
+        public int CreatePlayerId { get; set; }
+        public int WonPlayerId { get; set; }
         public enum SessionStatus
         {
             Waiting,
